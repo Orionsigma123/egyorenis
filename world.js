8786581e55e0fa91420e15b96e28c7647b58edd9
Orionsigma123 <sigmaorion77@gmail.com>
2024-10-20 16:45:03 -0400

    Update world.js

--- world.js
+++ world.js
@@ -2,26 +2,33 @@ class World {
     constructor() {
         this.blocks = [];
         this.blockSize = 1;
-        this.worldSize = 10; // Size of the generated world
+        this.worldSize = 10;
         this.createWorld();
     }
 
-    createWorld() {
+    createBlock(type, position) {
         const geometry = new THREE.BoxGeometry(this.blockSize, this.blockSize, this.blockSize);
         const materials = {
             grass: new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('textures/grass.jpg') }),
             dirt: new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('textures/dirt.png') }),
             stone: new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('textures/stone.png') }),
+            water: new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('textures/water.png') }),
+            wood: new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('textures/wood.png') }),
         };
 
+        const block = new THREE.Mesh(geometry, materials[type]);
+        block.position.copy(position);
+        return block;
+    }
+
+    createWorld() {
         for (let x = -this.worldSize; x <= this.worldSize; x++) {
             for (let z = -this.worldSize; z <= this.worldSize; z++) {
-                const height = Math.floor(Math.random() * 3); // Random height for demo purposes
+                const height = Math.floor(Math.random() * 3);
 
                 for (let y = 0; y <= height; y++) {
                     const blockType = y === height ? 'grass' : (y > 0 ? 'dirt' : 'stone');
-                    const block = new THREE.Mesh(geometry, materials[blockType]);
-                    block.position.set(x * this.blockSize, y * this.blockSize, z * this.blockSize);
+                    const block = this.createBlock(blockType, new THREE.Vector3(x * this.blockSize, y * this.blockSize, z * this.blockSize));
                     this.blocks.push(block);
                 }
             }
